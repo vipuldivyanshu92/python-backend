c88cb11be82e4cde846692b1acd858c53bd798d6
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-10 15:52:14 +0530

    1

--- modules/user_actions_lib.py
+++ modules/user_actions_lib.py
@@ -1,5 +1,18 @@
-from pymongo import Connection
+# from pymongo import Connection
 #import
 
-connection = Connection()
-db=connection.giffie.users_trans
+#mongo db create
+# connection = Connection()
+# db=connection.Giffie.users_trans
+
+AUTH_ID=''
+
+def user_online_ping(auth_id,db,nick_timestamp):
+  
+  
+def user_get_msgs_ping(auth_id,db,nick_timestamp):
+  
+
+def user_is_contact_registered(auth_id,db,nick_timestamp,phone_num):
+  
+def user_post(auth_id,db,nick_timestamp,phone_num):
