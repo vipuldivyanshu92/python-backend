716a0770599b2803296d9907f611a4ca754989f4
Vipul DIvyanshu <vipuldivyanshu6.11@gmail.com>
2014-10-17 16:46:43 +0530

    Update user_actions_lib.py

--- modules/user_actions_lib.py
+++ modules/user_actions_lib.py
@@ -7,12 +7,18 @@
 
 AUTH_ID=''
 
-def user_online_ping(auth_id,db,nick_timestamp):
+def user_online_ping(auth_id,db,nick):
   
   
-def user_get_msgs_ping(auth_id,db,nick_timestamp):
+def user_get_msgs_ping(auth_id,db,nick,iemi):
   
-
-def user_is_contact_registered(auth_id,db,nick_timestamp,phone_num):
+  
+def user_name_registered(auth_id,db,nick): # this is to check if the freinds name entered registered or not
+  
+  
+def user_is_contact_registered(auth_id,db,nick,phone_num,email):# this is to check if the user is contact is registered or not
+  
   
 def user_post(auth_id,db,nick_timestamp,phone_num):
+  
+def user_receive_confirm(auth_id,db,id,iemi,systimestamp)
